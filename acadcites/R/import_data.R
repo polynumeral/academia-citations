24bb707f8e3cc571fa692a0dc887e6d56472bddf
Carl Vogel <carljv@gmail.com>
2015-07-07 22:03:10 -0400

    Fix bug resulting from using import args as positional; move them to implicit (...)

--- acadcites/R/import_data.R
+++ acadcites/R/import_data.R
@@ -7,13 +7,12 @@
 #' @param journals_path A path to a file with data on academic journals. (defaults to package dataset)
 #' @param doc_types_dir A path to a file with document type classification results
 #' (Original Research, Commentary/Editorial, etc.) from Amazon MTurk. (defaults to package dataset)
-#' @param only_complete (default TRUE) Only keep rows where data is not missing.
-#' @param only_original (default TRUE) Only keep rows with "Original Research" articles.
+#' @param ... Optional arguments to import and joining functions.
 #' 
 #' @return A dataframe of citation, download, and journal data merged together.
 #'
 importData <- function(cites_path=NULL, journals_path=NULL, doc_types_path=NULL,
-                      only_complete=TRUE, only_original=TRUE) {
+                      only_complete=TRUE, only_original=TRUE, ...) {
 
     if (is.null(cites_path)) {
         cites_path <- system.file('extdata', 'papers.csv.gz',
@@ -30,15 +29,14 @@ importData <- function(cites_path=NULL, journals_path=NULL, doc_types_path=NULL,
     }
 
     cat('Importing article citations ...\n')
-    cites <- importCitesFile(cites_path, only_complete)
+    cites <- importCitesFile(cites_path, ...)
     cat('Importing journal information ...\n')
-    journals <- importEraFile(journals_path)
+    journals <- importEraFile(journals_path, ...)
     cat('Importing document types ...\n')
-    doc_types <- importDocTypes(doc_types_path)
+    doc_types <- importDocTypes(doc_types_path, ...)
     
-    cites <- joinCitesJournals(cites, journals)
-    cites <- joinCitesDocTypes(cites, doc_types, only_original)
-
+    cites <- joinCitesJournals(cites, journals, ...)
+    cites <- joinCitesDocTypes(cites, doc_types, ...)
     cites
 }
 
@@ -150,7 +148,7 @@ joinCitesJournals <- function(cites_df, journals_df, ...) {
 joinCitesDocTypes <- function(cites_df, classifications_df, only_original=TRUE, ...) {
     df <-left_join(cites_df, classifications_df, by='be_cf_id')
     if (only_original) {
-        df %>%filter(document_type == 'Original Research')
+        df %>% filter(document_type == 'Original Research')
     } else {
         df
     }
